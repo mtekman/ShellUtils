559c8666a6e9c74163196ee007df1c3ace8f0605
Mehmet Tekman <m.tekman@ucl.ac.uk>
2015-05-19 18:05:51 +0100

    still working on filling gaps

--- optimal_setup.sh
+++ optimal_setup.sh
@@ -9,14 +9,37 @@ function largestRez() {
 
 
 function winGeom(){
-	if [ "$1" != "" ]; then
-		xdotool windowmove $1 $2 $3
-		xdotool windowsize $1 $4 $5
-	fi
+	nam=$1
+	[ "$1" != "" ] && xdotool windowmove $nam $2 $3	# positions
+	[ "$4" != "" ] && xdotool windowsize $nam $4 $5 # dimension
 }
 
 
+# Grabs the earliest process
+rez=1
+function xsearch(){
+	rez=1
+	win="$(xdotool search --onlyvisible --name $1)"
+	rez=$?
+	echo "$win" | sort | head -1
+}
 
+
+# Polls until exists
+function waitWindow(){
+	name=$1
+	check_once=$2
+
+	win=$(xsearch $name)
+	if [ "$check_once" != "" ]; then
+		echo $win
+	else
+		while [ "$rez" = "1" ] || [ "$win" = "" ]; do win=$(xsearch $name); sleep 1; done
+		echo $win
+	fi
+}
+
+# Globals
 rez=$(largestRez)
 
 width=$(echo $rez | awk '{print $1}')
@@ -24,24 +47,36 @@ height=$(echo $rez | awk '{print $2}')
 
 h_w=$(echo "$width / 2" | bc)
 h_h=$(echo "$height / 2" | bc)
-#echo $h_w $h_h
-#exit
-
-mplayer_win=$(xdotool search --name mplayer2)
-firefox_win=$(xdotool search --name Mozilla)
-lightab_win=$(xdotool search --name "Sublime")
-
-[ "$firefox_win" = "" ] && firefox & pid1=$!
-[ "$lightab_win" = "" ] && ~/Programs/Sublime\ Text\ 2/sublime_text & sleep 5
-
-[ "$pid1" != "" ] && wait $pid1
 
 
-firefox_win=$(xdotool search --name Mozilla)
-lightab_win=$(xdotool search --name "Light Table")
+# Programs
+firefox=firefox
+sublime=~/Programs/Sublime_Text_2/sublime_text
+pomodor=tomate
 
-winGeom $mplayer_win 0 0 $h_w $h_h 2>/dev/null
+# Initial scan
+f_win=$(waitWindow Mozilla 1)
+s_win=$(waitWindow Sublime 1)
+p_win=$(waitWindow Tomate 1)
+
+# Spawn those not up
+[ "$f_win" = "" ] && $firefox &
+[ "$s_win" = "" ] && $sublime &
+[ "$p_win" = "" ] && $pomodor &
+
 sleep 1
-winGeom $firefox_win 0 $h_h $h_w $h_h
+
+# Poll until exists
+f_win=$(waitWindow Mozilla)
+s_win=$(waitWindow Sublime)
+p_win=$(waitWindow Tomate)
+
+
+winGeom $s_win 0 0 $h_w $height
 sleep 1
-winGeom $lightab_win $h_w 0 $h_w $height
+
+y_div=$(( $height - 100 ))
+
+winGeom $f_win $h_w 0 $h_w $y_div
+sleep 1
+winGeom $p_win $h_ $y_div
